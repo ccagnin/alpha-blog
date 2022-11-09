91a24696d55e7822de9df9390036cc951dd3b4b5
Caroline Cagnin <cagnin.caroline@gmail.com>
2022-11-09 16:54:19 -0300

    feature: logged in and out restrictions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,13 @@ module ApplicationHelper
         robot_url = "https://robohash.org/#{hash}.png/bgset_any?size=#{size}"
         image_tag(robot_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
      
-      end
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
 end
